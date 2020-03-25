a34719701099c558cbf72139412ebe20f80fd426
Anthony Mendoza <anthonymendoza9922@gmail.com>
2020-03-25 04:01:26 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,15 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+new_hash = {}
+data.each do |bird , value|
+  value.each do |section,array|
+    array.each do |name|
+       if !new_hash.has_key?(name)
+          new_hash[name] = {}
+      end
 end
+end
+end
+new_hash
+end
+
+
