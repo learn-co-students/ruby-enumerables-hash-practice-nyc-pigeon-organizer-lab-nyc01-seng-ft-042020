25e2dcb7611200ec4b977af32eeeda915640a910
Elias Taveras <eliastave12@gmail.com>
2020-03-26 21:14:24 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,21 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+    pigeon_hash = {}
+    data.each do |property, hash|
+        hash.each do |attribute, array|
+            array.each do |name|
+                if !pigeon_hash.has_key?(name)
+                    pigeon_hash[name] = {}
+                end
+                
+                if !pigeon_hash[name].has_key?(property)
+                    pigeon_hash[name][property] = []
+                end
+                
+                if !pigeon_hash[name][property].include?(attribute)
+                    pigeon_hash[name][property] << attribute.to_s
+                end
+            end
+        end
+    end
+    pigeon_hash
 end
