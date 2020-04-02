4360165ce71a1a7c770a730943ab15f03e0871ca
Alice Wang <aywang6@gmail.com>
2020-04-02 16:23:39 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,15 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  
+  data_by_pigeon = {}
+  
+  data.keys.each do |property| 
+    data[property].keys.each do |value|
+      data[property][value].each do |name|
+        data_by_pigeon[name] = data_by_pigeon[name] || {}  
+        data_by_pigeon[name][property] = data_by_pigeon[name][property] || []
+        data_by_pigeon[name][property] << value.to_s 
+      end
+    end
+  end
+ data_by_pigeon
 end
