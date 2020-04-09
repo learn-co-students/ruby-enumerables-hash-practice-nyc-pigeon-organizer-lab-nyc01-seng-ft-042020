42c611d526dfbc5c166f0bfb9484640edc42cfcc
Don Namgyal <dhondup.namgyal@gmail.com>
2020-04-09 17:31:11 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,15 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  updated_data = {}
+  data.each do |attributes, value_hash|
+    value_hash.each do |type, names_array|
+      names_array.each do |name|
+        updated_data[name] ||= {}
+        updated_data[name][attributes] ||= []
+        updated_data[name][attributes] << type.to_s
+      end
+    end
+  end
+  updated_data
 end
