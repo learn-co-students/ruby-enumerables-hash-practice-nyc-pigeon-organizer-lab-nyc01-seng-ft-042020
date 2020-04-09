45a283e12ae7b53e77ff384fe33694a2f8d8dbd1
dnamgyal <don.namgyal@hotmail.com>
2020-04-09 14:06:18 -0400

    Update nyc_pigeon_organizer.rb

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -5,9 +5,15 @@ def nyc_pigeon_organizer(data)
   data.each do |attributes, value_hash|
     value_hash.each do |type, names_array|
       names_array.each do |name|
-        updated_data[name] ||= {}
-        updated_data[name][attributes] ||= []
-        updated_data[name][attributes] << type.to_s
+        if !updated_data[name]
+          updated_data[name] = {}
+        end
+
+        if !updated_data[name][attributes]
+          updated_data[name][attributes] = []
+        end
+
+        updated[name][attributes] << type.to_s
       end
     end
   end
